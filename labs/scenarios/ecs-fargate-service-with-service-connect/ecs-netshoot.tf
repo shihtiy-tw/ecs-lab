0bd0d8eafc027db4c1e2e886822bb86fe1b213b3
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2024-12-24 11:51:31 +0000

    feat(scenario): add service connect example

---
+++ labs/scenarios/ecs-fargate-service-with-service-connect/ecs-netshoot.tf
@@ -0,0 +1,49 @@
+resource "aws_ecs_task_definition" "netshoot" {
+  family                   = "${data.aws_ecs_cluster.cluster.cluster_name}-fargate-netshoot-${var.scenario_name}"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.task_cpu
+  memory                   = var.task_memory
+  execution_role_arn       = aws_iam_role.ecs_tasks_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_tasks_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name              = "netshoot"
+      image             = var.container_image_netshoot
+      memoryReservatino = 128
+      essential         = true
+      entrypoint        = ["sh", "-c"]
+      # command           = ["while true; do curl -I ${aws_lb.main.dns_name}; done"]
+      command = ["sleep 7200"]
+    }
+  ])
+
+  # depends_on = [
+  #   aws_lb.main
+  # ]
+}
+
+resource "aws_ecs_service" "service_netshoot" {
+  name            = "${data.aws_ecs_cluster.cluster.cluster_name}-service-netshoot-${var.scenario_name}"
+  cluster         = data.aws_ecs_cluster.cluster.arn
+  task_definition = aws_ecs_task_definition.netshoot.arn
+  # desired_count          = var.service_desired_count
+  desired_count          = 1
+  enable_execute_command = true
+  launch_type            = "FARGATE"
+
+  service_connect_configuration {
+    enabled   = true
+    namespace = aws_service_discovery_private_dns_namespace.private.arn
+  }
+
+  network_configuration {
+    subnets         = data.aws_subnets.private_subnets.ids
+    security_groups = [aws_security_group.ecs.id]
+  }
+
+  depends_on = [
+    aws_ecs_service.main
+  ]
+}
---
+++ labs/scenarios/ecs-fargate-service-with-service-connect/service-discovery.tf
@@ -0,0 +1,24 @@
+resource "aws_service_discovery_private_dns_namespace" "private" {
+  name        = "${var.domain_name}-${var.scenario_name}"
+  description = "Private dns namespace for service discovery"
+  vpc         = data.aws_vpc.vpc.id
+}
+
+# resource "aws_service_discovery_service" "nginx" {
+#   name = var.service_name_nginx
+#
+#   dns_config {
+#     namespace_id = aws_service_discovery_private_dns_namespace.private.id
+#
+#     dns_records {
+#       ttl  = var.ttl
+#       type = var.type
+#     }
+#
+#     routing_policy = var.routing_policy
+#   }
+#
+#   health_check_custom_config {
+#     failure_threshold = 1
+#   }
+# }
